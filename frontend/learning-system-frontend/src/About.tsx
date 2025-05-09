bfa37cf0c99faf23be5b0d4077e79ce0cca95530
verysvadym <veresivadim3@gmail.com>
2025-05-09 14:17:36 +0200

    Were added About.tsx, Contact.tsx, Home.tsx, Navbar.tsx

---
+++ frontend/learning-system-frontend/src/About.tsx
@@ -0,0 +1,12 @@
+import Navbar from './Navbar'
+
+const About = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>About</h1>
+        </div>
+    )
+}
+
+export default About
---
+++ frontend/learning-system-frontend/src/Contact.tsx
@@ -0,0 +1,12 @@
+import Navbar from './Navbar'
+
+const Contact = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>Contact</h1>
+        </div>
+    )
+}
+
+export default Contact

514b6f192a3e8867634580bff920b909b3133356
ondychrbi <ondrej.chrbolka@icloud.com>
2025-05-18 18:03:22 +0200

    Teachers notes - Konzultace 04
    Zarovnání obsahu stránky na top.

--- frontend/learning-system-frontend/src/App.tsx
+++ frontend/learning-system-frontend/src/App.tsx
@@ -13,7 +13,7 @@ import Users from "./Users.tsx";
 const App = () => {
 
     return (
-        <div className="App" style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100vh", width: "100vw", backgroundColor: "#ffffff", textAlign: "center" }}>
+        <div className="App" style={{ display: "flex", flexDirection: "column", height: "100vh", width: "100vw", backgroundColor: "#ffffff", textAlign: "center" }}>
             <AppNavBar />
             <Routes>
                 <Route path='/' element={<Home/>} />

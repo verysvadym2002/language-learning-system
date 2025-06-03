6504c0c0bfb0100e09e1856f8ef5cf4e961f566d
verysvadym <veresivadim3@gmail.com>
2025-06-03 12:08:25 +0200

    Deleted unused functions in Navbar and added list of courses in Courses.tsx

--- frontend/learning-system-frontend/src/Courses.tsx
+++ frontend/learning-system-frontend/src/Courses.tsx
@@ -1,11 +1,12 @@
-import AppNavBar from "./AppBar.tsx";
+import CoursesTable from "./components/CoursesTable.tsx";
 
 const Courses = () => {
     return (
         <div>
-            <h1>About</h1>
+            <h1>Courses</h1>
+            <CoursesTable />
         </div>
-    )
-}
+    );
+};
 
-export default Courses
+export default Courses;
